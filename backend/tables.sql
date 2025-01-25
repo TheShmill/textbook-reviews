c7d0b5c7f3c056614fbc21b8d4b5e9cc1727b692
TheShmill <21013808+TheShmill@users.noreply.github.com>
2025-01-25 11:35:34 -0500

    Basic schema

--- backend/tables.sql
+++ backend/tables.sql
@@ -3,6 +3,25 @@ CREATE TABLE IF NOT EXISTS books(
     author TEXT NOT NULL,
     year INT NOT NULL,
     edition TEXT NOT NULL,
-    isbn TEXT NOT NULL,
+    isbn TEXT NOT NULL UNIQUE,
     authorid INT
+);
+
+CREATE TABLE IF NOT EXISTS users(
+    username TEXT NOT NULL UNIQUE,
+    displayname TEXT
+);
+
+CREATE TABLE IF NOT EXISTS reviews(
+    stars REAL NOT NULL,
+    bookid INT NOT NULL,
+    userid INT NOT NULL,
+    comment TEXT,
+    UNIQUE(bookid, userid)
+);
+
+CREATE TABLE IF NOT EXISTS likes(
+    userid INT NOT NULL,
+    reviewid INT NOT NULL,
+    UNIQUE(userid, reviewid)
 );
