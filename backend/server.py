71d9a467c9ce05d313beafe873695e9ed0fee562
TheShmill <21013808+TheShmill@users.noreply.github.com>
2025-01-25 14:11:19 -0500

    get_book_reviews

--- backend/server.py
+++ backend/server.py
@@ -14,6 +14,36 @@ def get_db():
     return db
 
 
+@app.route("/book/<int:book_id>/reviews")
+def get_book_reviews(book_id):
+    reviews = (
+        get_db()
+        .cursor()
+        .execute(
+            "SELECT stars, comment, userid FROM reviews WHERE bookid=?", (book_id,)
+        )
+        .fetchall()
+    )
+    data = []
+    for review in reviews:
+        stars = review[0]
+        comment = review[1]
+        userid = review[2]
+        user = (
+            get_db()
+            .cursor()
+            .execute("SELECT username, displayname FROM users WHERE rowid=?", (userid,))
+            .fetchone()
+        )
+        if user[1]:
+            name = user[1]
+        else:
+            name = user[0]
+        print(name)
+        data.append({"stars": stars, "comment": comment, "name": name})
+    return data
+
+
 @app.route("/book/<int:book_id>")
 def get_book(book_id):
     print(type(book_id))
