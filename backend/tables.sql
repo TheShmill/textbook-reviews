85db14e36139ed5f34d3ef8eada9d972126dd458
TheShmill <21013808+TheShmill@users.noreply.github.com>
2025-01-25 11:23:25 -0500

    Book schema

---
+++ backend/tables.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS books(
+    title TEXT NOT NULL,
+    author TEXT NOT NULL,
+    year INT NOT NULL,
+    edition TEXT NOT NULL,
+    isbn TEXT NOT NULL,
+    authorid INT
+);
