47976100130db6d9c07b4fda9f73f83030edeff4
TheShmill <21013808+TheShmill@users.noreply.github.com>
2025-01-26 07:16:28 -0500

    Example textbook data

---
+++ backend/sample_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO BOOKS
+VALUES
+    ("Structure and Interpretation of Computer Programs", "Harold Abelson, Gerald Sussman", 1984, "1st edition", "978-0070004221", NULL),
+    ("Structure and Interpretation of Computer Programs", "Harold Abelson, Gerald Sussman", 1996, "2nd edition", "0-262-51087-1", NULL),
+    ("Paradigms of AI Programming", "Peter Norvig", 1992, "1st edition", "1-55860-191-0", NULL),
+    ("Linear Algebra", "Stephen Friedberg", 2018, "5th edition", "978-0134860244", NULL),
+    ("Linear Algebra", "Stephen Friedberg", 2002, "4th edition", "978-0130084514", NULL),
+    ("Crafting Interpreters", "Robert Nystrom", 2021, "1st edition", "9780990582946", NULL),
+    ("Effective Java", "Joshua Bloch", 2018, "3rd edition", "9780134685991", NULL),
+    ("Effective Java", "Joshua Bloch", 2008, "2nd edition", "9780132778046", NULL),
+    ("A First Course in Database Systems", "Jeffrey Ullman and Jennifer Wildom", 2007, "3rd edition", "9781292038247", NULL);
